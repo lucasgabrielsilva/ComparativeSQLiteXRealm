f039ce3fe9bf172a8f80bbda628094de8c0713fa
pexee <lucas_gabrielsilva@hotmail.com>
2017-11-21 22:28:45 -0200

    Teste com 100 inserts realm

---
+++ appreact/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
+
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+
+    var x = require('./testerealm.js');
+    x.criarBanco();
+
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
---
+++ appreact/testerealm.js
@@ -0,0 +1,125 @@
+var exports = module.exports ={};
+
+
+    exports.criarBanco = function(){
+        const Realm = require('realm');
+        const now = require("performance-now");
+
+        class tabela8 {}
+        tabela8.schema ={
+            name: 'tabela8',
+            properties: {
+                id: 'int',
+                nome: 'string',
+            }
+        };
+
+        let realm = new Realm({schema: [tabela8]});
+
+        var start = now();
+        realm.write(() => {
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+            realm.create('tabela8', {id: 2, nome: 'pexe'});
+        });
+        var end = now();
+        let r = realm.objects('tabela8').filtered('id = 2');
+        alert('inicio: ' + start + '\nfim: ' + end + '\ndiferenca: ' + (end - start) + '\nqtde dados' + r.length);
+};
