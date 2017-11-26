be7e9bc5b7cf6b8058b405610d2f557b32a2cbe6
pexee <lucas_gabrielsilva@hotmail.com>
2017-11-26 15:23:59 -0200

    Atualização metodo inserts

--- appsqlite/testeSqlite.js
+++ appsqlite/testeSqlite.js
@@ -10,15 +10,18 @@ var exports = module.exports ={}
 		var db = SQLite.openDatabase('bancoSqlite');
 
 	    db.transaction(function(tx){
-	      tx.executeSql("CREATE TABLE tabela1 (id INTEGER, nome VARCHAR)")
+	      tx.executeSql('CREATE TABLE tabela1 (id INTEGER PRIMARY KEY, a VARCHAR, b VARCHAR, c VARCHAR, d VARCHAR, e VARCHAR' +
+	      	', f VARCHAR, g VARCHAR, h VARCHAR, i VARCHAR, j VARCHAR, k VARCHAR, l VARCHAR, m VARCHAR' + 
+	      	', n VARCHAR, o VARCHAR, p VARCHAR, q VARCHAR, r VARCHAR, s VARCHAR, t VARCHAR, u VARCHAR' + 
+	      	', v VARCHAR, x VARCHAR, w VARCHAR, y VARCHAR, z VARCHAR)');
 	    });
 
 //	    var start = now();
 		console.log(' ---- Inicio da operação de ' + numInserts + ' Inserts ----');
 	    for(var i = 0; i < numInserts; i++){
 	    	db.transaction(function(tx){
-	      		tx.executeSql('INSERT INTO tabela1 VALUES(?,?)', [i,nome]);
-	      		console.log(" ---- Conclusão do inserts numero:  " + j++ + " ---- ");
+	      		tx.executeSql('INSERT INTO tabela1 VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [j++,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome,nome]);
+	      		console.log(" ---- Conclusao do inserts numero:  " + j + " ---- ");
 	    	});
 	    }
 
